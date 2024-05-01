93350a9eacadf720c14a9a2e2109fadfbc3d3856
acapria <131828886+acapria@users.noreply.github.com>
2024-05-01 09:35:39 -0700

    Update hostflags.py

--- hostflags.py
+++ hostflags.py
@@ -1,20 +1,20 @@
 # hostflags.py
 import csv
-
-def read_hostnames_from_csv(filename):
+def read_hostnames_from_tsv(filename):
     hostnames = []
     with open(filename, 'r') as file:
-        reader = csv.reader(file)
+        reader = csv.reader(file, delimiter='\t')
         for row in reader:
             hostnames.extend(row)
     return hostnames
 
 def determine_flag(hostname):
-    labhost_file = "lab_host.csv"
-    isolation_file = "isolation_source.csv"
+    labhost_file = "lab_host.tsv"
+    isolation_file = "isolation_source.tsv"
     
-    labhost_list = read_hostnames_from_csv(labhost_file)
-    isolation_list = read_hostnames_from_csv(isolation_file)
+    labhost_list = read_hostnames_from_tsv(labhost_file)
+    isolation_list = read_hostnames_from_tsv(isolation_file)
+
 
     # Check if hostname is in labhost_list
     if hostname in labhost_list:
