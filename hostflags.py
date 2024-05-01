d2c90d3c38b983336b7a82264e2e1154d53fb0d7
acapria <131828886+acapria@users.noreply.github.com>
2024-05-01 09:49:15 -0700

    Update hostflags.py
    added prints to check it works

--- hostflags.py
+++ hostflags.py
@@ -15,17 +15,21 @@ def determine_flag(hostname):
     labhost_list = read_hostnames_from_tsv(labhost_file)
     isolation_list = read_hostnames_from_tsv(isolation_file)
 
-
-    # Check if hostname is in labhost_list
+        # Check if hostname is in labhost_list
     if hostname in labhost_list:
+        print ("FLAG (Possible_labhost)")
         return "FLAG (Possible_labhost)"
+        
 
     # Check if hostname is in isolation_list
     elif hostname in isolation_list:
+        print ("FLAG (Possible_IsoSource)")
         return "FLAG (Possible_IsoSource)"
+        
 
     # If hostname is not in either list, return No_matching_Host flag
     else:
+        print ("FLAG (No_matching_Host)")
         return "FLAG (No_matching_Host)"
 
 # Example usage:
